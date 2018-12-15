361fc10b68cda8d61a75d8709511bce5a4a1b756
Dominik M. Kwiatek <dom@dom.dog>
2018-12-15 17:26:51 +0000

    Initial Commit

---
+++ LC3/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LC3 {
+    internal static class Program {
+        private static void Main(string[] args) {
+            Console.WriteLine("Hello Dög!");
+        }
+    }
+}
