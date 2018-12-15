57c06b76d40937ccfbb47e789eb5013663b07ecb
Dominik M. Kwiatek <dom@dom.dog>
2018-12-15 22:44:30 +0000

    Forgot to increment PC

--- LC3/Processor.cs
+++ LC3/Processor.cs
@@ -17,8 +17,13 @@ namespace LC3 {
             set => this[(int)key] = value;
         }
 
+        public void Increment(Register register) {
+            this[register]++;
+        }
+
         public void Fetch() {
             this[Register.MemoryAddress] = this[Register.ProgramCounter];
+            Increment(Register.ProgramCounter);
             this[Register.MemoryData] = Memory[this[Register.MemoryAddress]];
             var instruction = this[Register.MemoryData];
         }
