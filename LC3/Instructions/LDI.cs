acd4a173fc73896b546c2cd52df1e9a6a2ad79d0
Dominik M. Kwiatek <dom@dom.dog>
2018-12-25 14:56:00 +0000

    Implement LDI

---
+++ LC3/Instructions/LDI.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace LC3.Instructions {
+    public class LDI : IInstruction {
+        public void Call(Processor processor) {
+            var mdr = processor[Register.MemoryData];
+            var register = (Register)(mdr >> 9 & 0b111);
+            var ptr = mdr & 0b1_1111_1111;
+            processor[register] = processor.Memory[processor.Memory[ptr]];
+
+            if (Program.Disassemble) {
+                Console.WriteLine($"LDI\t{register}, ${ptr:X}");
+            }
+        }
+    }
+}
