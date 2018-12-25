a89b217fa72a94365a3aa08ecca42622346b2d65
Dominik M. Kwiatek <dom@dom.dog>
2018-12-25 16:05:14 +0000

    Add cache for opcodes instead of using reflection every time

--- LC3/Instruction.cs
+++ LC3/Instruction.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
 namespace LC3 {
     public class Instruction {
+        public static Dictionary<OpCode, IInstruction> opcodeMap = new Dictionary<OpCode, IInstruction>();
+        
         public static IInstruction Get(ushort opcode) {
             return Get((OpCode) opcode);
         }
@@ -13,10 +16,14 @@ namespace LC3 {
         }
 
         public static IInstruction Get(OpCode opcode) {
-            var types = Assembly.GetExecutingAssembly().GetTypes();
-            var type = types.FirstOrDefault(t => t.Name == opcode.ToString()) ??
-                       throw new NotImplementedException($"Opcode not implemented: {opcode}");
-            return (IInstruction) Activator.CreateInstance(type);
+            if (!opcodeMap.ContainsKey(opcode)) {
+                var types = Assembly.GetExecutingAssembly().GetTypes();
+                var type = types.FirstOrDefault(t => t.Name == opcode.ToString()) ??
+                           throw new NotImplementedException($"Opcode not implemented: {opcode}");
+                opcodeMap[opcode] = (IInstruction) Activator.CreateInstance(type);
+            }
+
+            return opcodeMap[opcode];
         }
     }
 }
